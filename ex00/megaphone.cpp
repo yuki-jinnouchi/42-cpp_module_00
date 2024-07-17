18a658407df238c9dbe246199b023eaa21f743fa
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2024-07-17 23:02:39 +0200

    update ex00: delete 42 header

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,18 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/17 15:32:16 by yjinnouc          #+#    #+#             */
-/*   Updated: 2024/07/17 20:45:25 by yjinnouc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-// My coding style is based in C++ Coding Standard by Todd Hoff:
-// http://maultech.com/chrislott/resources/cstyle/CppCodingStandard.html
-
 #include <iostream>
 #include <string>
 
@@ -20,40 +5,40 @@ using namespace std;
 
 string to_upper(string str)
 {
-    int     i;
-    int     strlen;
+	int     i;
+	int     strlen;
 
-    i = 0;
-    strlen = str.length();
-    while (i < strlen)
-    {
-        str[i] = toupper(str[i]);
-        i++;
-    }
-    return (str);
+	i = 0;
+	strlen = str.length();
+	while (i < strlen)
+	{
+		str[i] = toupper(str[i]);
+		i++;
+	}
+	return (str);
 }
 
 void megaphone(int argc, char **argv)
 {
-    int     i;
-    string  str;
+	int     i;
+	string  str;
 
-    i = 1;
-    while (i < argc)
-    {
-        str = argv[i];
-        str = to_upper(str);
-        cout << str;
-        i++;
-    }
-    cout << endl;
+	i = 1;
+	while (i < argc)
+	{
+		str = argv[i];
+		str = to_upper(str);
+		cout << str;
+		i++;
+	}
+	cout << endl;
 }
 
 int main(int argc, char **argv) {
-    if (argc > 1) {
-        megaphone(argc, argv);
-    } else {
-	    cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
-    }
-    return (0);
+	if (argc > 1) {
+		megaphone(argc, argv);
+	} else {
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	}
+	return (0);
 }
